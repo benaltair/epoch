b1c25beeaeabb3d0427f1a8341fcc16740561661
Ben Altair <ben@benaltair.com>
2023-09-25 23:29:41 -0700

    Collapse components for now, create global timeline CSS grid dynamically adjusted to params

--- src/lib/Timeline/Navigator.svelte
+++ src/lib/Timeline/Navigator.svelte
@@ -2,7 +2,6 @@
 	let significantDates = [1844, 1853, 1892, 1921, 2016, 2021, 2044];
 	export let firstYear: number;
 	export let lastYear: number;
-	let navWidth: number;
 
 	function getDecades(firstYear: number, lastYear: number): number[] {
 		if (firstYear > lastYear) {
@@ -25,40 +24,60 @@
 	$: {
 		decadesArray = getDecades(firstYear, lastYear);
 	}
+
+
 </script>
 
 <!-- @component
 Provides a navigation bar at the bottom of the page with the timeline.
  -->
-
-<nav
-	bind:clientWidth={navWidth}
+<main
 	style="--first-year:{firstYear};--last-year:{lastYear};--number-of-decades:{decadesArray.length};"
 >
-	<!-- Listing all the significant dates -->
-	{#each decadesArray as year}
-		<time datetime={new Date(year).toDateString()}>
-			{year}
-		</time>
-	{/each}
-</nav>
+	<nav>
+		<!-- Listing all the significant dates -->
+		{#each decadesArray as year}
+			<time datetime={new Date('1 January ' + year).toISOString()}>
+				{year}
+			</time>
+		{/each}
+	</nav>
+</main>
 
 <style>
+	main {
+		--spacing: 2.5em;
+		--inner-height: calc(100vh - var(--spacing) * 2);
+		--inner-width: calc(100vw - var(--spacing) * 2);
+		--timeline-grid: repeat(
+			var(--number-of-decades),
+			calc(var(--inner-width) / var(--number-of-decades))
+		);
+		display: grid;
+		align-content: baseline;
+		position: relative;
+		width: 100vw;
+		height: 100vh;
+		background-color: var(--bg);
+		display: grid;
+		padding: var(--spacing);
+		/* Create one column for each decade */
+		grid-template-columns: var(--timeline-grid);
+	}
 	nav {
 		position: absolute;
 		display: grid;
 		/* Create one column for each decade */
-		grid-template-columns: repeat(var(--number-of-decades), calc(100% / var(--number-of-decades)));
-		--side-margin: 2.5em;
-		width: calc(100vw - var(--side-margin) * 2);
-		height: var(--side-margin);
+		grid-template-columns: var(--timeline-grid);
+		width: calc(100vw - var(--spacing) * 2);
+		height: var(--spacing);
 		left: 0;
 		right: 0;
 		bottom: 0;
-		margin: var(--side-margin);
-		padding: 0.2em 0.9em;
-		background: var(--bg);
-		border-radius: calc(var(--side-margin) / 2);
+		margin: var(--spacing);
+		padding: 0.2em 0;
+		background: var(--accent-bg);
+		border-radius: calc(var(--spacing) / 8);
 	}
 	time {
 		width: max-content;
@@ -73,7 +92,7 @@ Provides a navigation bar at the bottom of the page with the timeline.
 		content: '';
 		display: block;
 		width: 0.2ch;
-		height: calc(var(--side-margin) / 6);
+		height: calc(var(--spacing) / 6);
 		background-color: rgb(138, 138, 138); /* Adjust the color of the dash as needed */
 		margin: 0 auto 0.2ch auto; /* Center the dash horizontally, adding a small margin below it */
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-	import Container from '$lib/Timeline/Container.svelte';
 	import Navigator from '$lib/Timeline/Navigator.svelte';
 </script>
 
-<Container>
-	<Navigator firstYear={1844} lastYear={2044} />
-</Container>
+<Navigator firstYear={1844} lastYear={2004} />
