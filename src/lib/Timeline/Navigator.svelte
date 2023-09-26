27c8830bf64b59d142cfc94ca8cf8887ba178d92
Ben Altair <ben@benaltair.com>
2023-09-25 22:36:24 -0700

    Add some base styles TODO: Remove additiona superfluous styling from simple.css

--- src/lib/Timeline/Navigator.svelte
+++ src/lib/Timeline/Navigator.svelte
@@ -21,7 +21,6 @@
 		return decades;
 	}
 
-	// Example usage:
 	let decadesArray: number[] = [];
 	$: {
 		decadesArray = getDecades(firstYear, lastYear);
@@ -50,14 +49,16 @@ Provides a navigation bar at the bottom of the page with the timeline.
 		display: grid;
 		/* Create one column for each decade */
 		grid-template-columns: repeat(var(--number-of-decades), calc(100% / var(--number-of-decades)));
-		--side-margin: 2em;
+		--side-margin: 2.5em;
 		width: calc(100vw - var(--side-margin) * 2);
 		height: var(--side-margin);
 		left: 0;
 		right: 0;
 		bottom: 0;
 		margin: var(--side-margin);
-		background-color: #7dc0ff9e;
+		padding: 0.2em 0.9em;
+		background: var(--bg);
+		border-radius: calc(var(--side-margin) / 2);
 	}
 	time {
 		width: max-content;
@@ -65,5 +66,15 @@ Provides a navigation bar at the bottom of the page with the timeline.
 		text-align: center;
 		align-self: center;
 		justify-self: center;
+		margin-top: -0.5ch;
+		color: var(--text-light);
+	}
+	time::before {
+		content: '';
+		display: block;
+		width: 0.2ch;
+		height: calc(var(--side-margin) / 6);
+		background-color: rgb(138, 138, 138); /* Adjust the color of the dash as needed */
+		margin: 0 auto 0.2ch auto; /* Center the dash horizontally, adding a small margin below it */
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,5 @@
+<script lang="ts">
+	import '$lib/styles/simple.css';
+</script>
+
 <slot />
